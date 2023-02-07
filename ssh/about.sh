d8aee1c490a1b3a6cbe4cf33f2b5bc0c212574fd
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-02-07 09:32:48 +0800

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#     Premium Auto Script By Sha Eccha      #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By Sha Eccha                     #"
+echo -e "# MOD By NevermoreSSH                                 #"
+echo -e "================================================="
+echo -e "# Thanks To                                     #"
+echo -e "================================================="
+echo -e "# KucingOyen                                     #"
+echo -e "# My Family                                     #"
+echo -e "# VPNtodfix                                        #"
+echo -e "# Terbaik                                       #"
+echo -e "================================================="
